70f3832e37a974375a93718ad090ff55862481f1
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2022-03-29 16:03:35 +0530

    Create ex3.java

---
+++ Unit 1/ex3.java
@@ -0,0 +1,14 @@
+//printing values
+
+class ex3 {
+    public static void main(String[] args) 
+    {
+        int number = 50;
+        double salary = 4562.32;
+        float rate = 18.2f;
+
+        System.out.println("Number : " + number);
+        System.out.println("Salary : " + salary);
+        System.out.println("Rate : " + rate);
+    }
+}
