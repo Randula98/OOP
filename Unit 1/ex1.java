9d925668ac9d0435e4ca452438919b30c40b95e1
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2022-03-29 15:55:03 +0530

    ex1 and 2

---
+++ Unit 1/ex1.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[])
+    {
+        /*first two parts will be printed on the same line
+        hello
+        java*/
+        //comment
+        System.out.print("Hello World.......");
+        System.out.println(" Welcome to Java   ");
+        System.out.print("Enjoy");
+    }
+}
---
+++ Unit 1/ex2.java
@@ -0,0 +1,15 @@
+class ex2 {
+    public static void main(String[] args) 
+    {
+        System.out.println("Randula");
+        System.out.println("Galigamuwa");
+        System.out.println("Kegalle");
+
+        System.out.println("\n\n");
+
+        System.out.print("Randula ");
+        System.out.println("23");
+        System.out.println("Galigamuwa");
+    }
+    
+}
