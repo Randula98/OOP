042f70d323667aa5d30e4f8992256229958a88c5
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2022-03-30 11:24:56 +0530

    Update ex4.java

---
+++ Unit 5/ex4.java
@@ -0,0 +1,12 @@
+class Main{
+	public static void main(String[] args) 
+	{
+		StringBuilder sb = new StringBuilder("I love Java");
+		
+		sb.substring(0, 9);
+		
+		sb = sb.reverse();
+		
+		System.out.println(sb);
+	}
+}
