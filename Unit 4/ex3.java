96368c9b9f5d7846643c2158ff98e9cfd2921700
Randula Yashasmith Mawaththa <85297495+Randula98@users.noreply.github.com>
2022-03-30 09:12:46 +0530

    compile time

---
+++ Unit 4/ex3.java
@@ -0,0 +1,26 @@
+class Main{
+	public static void main(String[] args) 
+	{
+		
+		int data[] = new int[5];
+		// 0 1 2 3 4 
+		
+		
+		//data[5] = 18;
+		
+		int value = 50;
+		
+		//int number = value / 0;
+		
+		//System.out.println(number);
+		
+		System.out.println();
+		
+		String name = "123a";
+		
+		int text = Integer.parseInt(name);
+		
+		System.out.println(text);
+
+	}
+}
